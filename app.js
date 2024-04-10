adb5cb1f33b1e003a67e9aeab37498fecec27e21
BellerOphoN697 <yesudasporathur@gmail.com>
2024-04-10 10:43:35 +0530

    modified:   app.js

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use('/admin',adminRouter);
 app.use('/', userRouter);
 
 app.listen(PORT,()=>{
-  console.log(`Server running in http://localhost:${PORT}`)
+  //console.log(`Server running in http://localhost:${PORT}`)
 })
 
 module.exports = app;
